09d9554ae005efe3ec77d6dc98aa2da4f248ad4a
Ritalexi <okoliogochukwu83@gmail.com>
2022-08-22 19:56:41 +0000

    update

---
+++ 0x00-python-hello_world/2-print.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+print('\"Proiramming is like building a multilinguai puzzle\n')
---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Holberton School"
+strnew = 3 * str
+strend = str[0:9]
+print(strnew + "\n" + strend + "\n")
