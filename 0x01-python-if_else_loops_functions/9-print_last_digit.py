40420ad30bd4b4668857d81c787d79e7cb7a572f
Ritalexi <okoliogochukwu83@gmail.com>
2022-08-24 22:25:20 +0000

    update

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if (number < 0):
+        r = "{}".format((number % -10) * -1)
+        print(r,end="")
+        return(r)
+    else:
+        r = "{}".format((number % 10))
+        print(r,end="")
+        return(r)
