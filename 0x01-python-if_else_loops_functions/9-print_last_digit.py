000443e321bdff39a8a5d20493e77ac0f46b789b
Ritalexi <okoliogochukwu83@gmail.com>
2022-08-24 22:34:19 +0000

    update

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,9 +2,9 @@
 def print_last_digit(number):
     if (number < 0):
         r = "{}".format((number % -10) * -1)
-        print(r,end="")
+        print(r, end="")
         return(r)
     else:
         r = "{}".format((number % 10))
-        print(r,end="")
+        print(r, end="")
         return(r)
