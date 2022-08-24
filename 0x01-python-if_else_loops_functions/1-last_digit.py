ddc1c7cfeeb03f21fdc9d2ede85da8de59faa503
Ritalexi <okoliogochukwu83@gmail.com>
2022-08-24 14:40:00 +0000

    update

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,5 +11,5 @@ if (last_digit > 5):
     print(reply, "and is greater than 5")
 elif (last_digit == 0):
     print(reply, "and is 0")
-elif (last_digit < 6 & last_digit != 0):
+else:
     print(reply, "and is less than 6 and not 0")
