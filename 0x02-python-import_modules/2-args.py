36d485737ed500e58989f69a044adbe61272d421
Ritalexi <okoliogochukwu83@gmail.com>
2022-08-25 22:00:10 +0000

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,9 @@
- #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-if __name__ == '__main__':
-    a = 10
-    b = 5
-    print("{}".format(a), "+", "{}".format(b), "=", "{}".format(add(a, b)))
-    print("{}".format(a), "+", "{}".format(b), "=", "{}".format(sub(a, b)))
-    print("{}".format(a), "+", "{}".format(b), "=", "{}".format(mul(a, b)))
-    print("{}".format(a), "+", "{}".format(b), "=", "{}".format(div(a, b)))
+#!/usr/bin/python3
+import sys
+len = len(sys.argv)
+if len == 1:
+    print("{}".format(len - 1), "arguments.")
+else:
+    print("{}".format(len - 1), "arguments:")
+    for i in range(1, len):
+        print("{}:".format(i), "{}".format(sys.argv[i]))
