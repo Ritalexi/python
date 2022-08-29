5a614fd6aa2a99c6ff4b98eed43b95248fffc95a
Ritalexi <okoliogochukwu83@gmail.com>
2022-08-29 17:39:53 +0000

    update

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,6 +4,6 @@ def element_at(my_list, idx):
         if (i == (idx):
             return my_list[i]
         elif (idx < 0):
-            return ("None")
-        elif(idx > len(my_list)):
-            return ("None")
+            return None
+        elif(idx > len(my_list) -1):
+            return None
