a8fbe0c77e8be3a03d3c8281f6e10f2e1fad926b
Ritalexi <okoliogochukwu83@gmail.com>
2022-09-05 22:48:17 +0000

    update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,5 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    max_key = list(a_dictionary.keys())
-    max_value = list(a_dictionary.values())
-    return (max_key[max_value.index(max(max_value))])
+    keyx = max(a_dictionary, key= lambda x: a_dictionary[x])
+    return keyx
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dictionary = a_dictionar.copy()
+    new_dictionary = a_dictionary.copy()
     for k, l in new_dictionary.items():
         new_dictionary[k] = l * 2
     return new_dictionary
