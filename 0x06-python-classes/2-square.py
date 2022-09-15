a445fc8af57d09ee5f064019f8704f6179f3f731
Ritalexi <okoliogochukwu83@gmail.com>
2022-09-15 22:37:38 +0000

    update

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a class Square that defines a square by
+"""
+class Square:
+    """nstantiation with optional size
+       raising exceptions
+    """
+    def __init__(self, size=0):
+        """Square instantiation and rise exxcetions
+        """
+        self.__size = size
+        
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
