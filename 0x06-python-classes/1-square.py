e83940684cce3ce8ab8899c0575b5b5985fa681a
Ritalexi <okoliogochukwu83@gmail.com>
2022-09-15 22:48:36 +0000

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,10 +2,10 @@
 """a class Square that defines a square by
 """
 
+
 class Square:
     """just simply installization of a class
     """
-
 
     def __init__(self, size):
         self.__size = size
