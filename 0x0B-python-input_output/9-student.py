24ec3d1315fe86d63f886459a6667dc7f9669a3d
Ritalexi <okoliogochukwu83@gmail.com>
2022-10-05 21:52:28 +0000

    update

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module that retrieves a dictionary representation
+"""
+
+
+class Student:
+    """
+    A class Studet
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Public Instantiation with
+        first_name, last_name and age
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        returns dictionary representation with simple data structure.
+        """
+       return self.__dict__
