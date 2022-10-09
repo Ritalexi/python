95fbdb78e8f09f08579c39e1b4490b4b4b292776
Ritalexi <okoliogochukwu83@gmail.com>
2022-10-09 23:03:48 +0000

    updaye

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,33 +24,64 @@ class Rectangle(Base):
         """Return private attribute"""
         return self.__width
     @width.setter
-    def width(self, width):
+    def width(self, value):
         """Setting private attribute"""
-        self.__width = width
+        self.validation("width", value)
+        self.__width = value
 
     @property
     def height(self):
         """Return private attribute"""
         return self.__height
     @height.setter
-    def height(self, height):
+    def height(self, value):
         """Setting private attribute"""
-        self.__height = height
+        self.validation("height", value)
+        self.__height = value
 
     @property
     def x(self):
         """Return private attribute"""
         return self.__x
     @x.setter
-    def x(self, x):
+    def x(self, value):
         """Setting private attribute"""
-        self.__x = x
+        self.validation("x", value)
+        self.__x = value
 
     @property
     def y(self):
         """Return private attribute"""
         return self.__y
     @y.setter
-    def y(self, y):
+    def y(self, value):
         """Setting private attribute"""
-        self.__y = y
+        self.validation("y", value)
+        self.__y = value
+
+
+    @staticmethod
+    def validation(attribute, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(attribute))
+        if attribute == "x" or attribute == "y":
+            if value < 0:
+                raise ValueError("{} must be >= 0".format(attribute))
+        elif value <= 0:
+            raise ValueError("{} must be > 0".format(attribute))
+
+    def area(self):
+        """
+        returns the area value of the Rectangle instance
+        """
+        return (self.__width * self.__height)
+
+    def display(self):
+        """
+        prints in stdout the Rectangle
+        instance with the character #
+        """
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#", end="")
+            print()
